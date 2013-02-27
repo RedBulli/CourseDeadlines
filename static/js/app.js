532059ec4b294dec64e53c145aea2b7004263c0a
Sampo Verkasalo <sampo.verkasalo@gmail.com>
2013-02-27 12:02:52 +0200

    example modal and search functionality from noppa-api

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,10 @@
 //Application logic
+COURSE_SEARCH_URL = 'http://noppa-api-dev.aalto.fi/api/v1/courses?key=cdda4ae4833c0114005de5b5c4371bb8&search=';
+var noppaCourseList = {};
+var searchedNames = [];
+
 $(document).ready(function() {
+  var search = new Search();
   var courses = new Courses();
   courses.fetch({
     success: function(collection) {
@@ -9,4 +14,31 @@ $(document).ready(function() {
       courseListView.render();
     }
   });
+  $('#newCourseModal').addClass('modal hide fade');
+  $('#courseNameSearch').bind('keyup', function(event) {
+    search.search($(this).val());
+  });
 });
+
+var Search = function() {
+  var _this = this;
+  _this.ajax = null;
+  _this.search = function(name) {
+    if ($.inArray(name, searchedNames) == -1) {
+      _this.ajax = $.getJSON(COURSE_SEARCH_URL+name+'&callback=?', function(data) {
+        $.each(data, function(index, course) {
+          noppaCourseList[course.course_id] = course;
+        });
+        updateDatalist();
+      });
+    }
+  };
+  return _this;
+}
+
+function updateDatalist() {
+  $('#courseDatalist').empty();
+  $.each(noppaCourseList, function(index, course) {
+    $('#courseDatalist').append('<option value="' + course.name + '">');
+  });
+}
