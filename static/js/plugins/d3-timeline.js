a1ee67fdb95248f14fa119fed97bedfc381de84e
Harri Lampi <harri.lampi@aalto.fi>
2013-04-23 14:23:54 +0300

    Testing function for touchdevices

--- static/js/plugins/d3-timeline.js
+++ static/js/plugins/d3-timeline.js
@@ -5,6 +5,7 @@
 
     var hover = function () {}, 
         click = function () {},
+        bind = function () {},
         scroll = function () {},
         orient = "bottom",
         width = null,
@@ -257,6 +258,12 @@
     timeline.click = function (clickFunc) {
       if (!arguments.length) return click;
       click = clickFunc;
+      return timeline;
+    };
+    
+    timeline.bind = function (bindFunc) {
+      if (!arguments.length) return bind;
+      bind = bindFunc;
       return timeline;
     };
     
