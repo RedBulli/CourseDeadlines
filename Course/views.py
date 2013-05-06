7184a1fabbde7b94795a98e40948e03148072341
Kaarle <kaarle.rajamaa@aalto.fi>
2013-05-06 11:04:27 +0300

    cleaning, save closes edit_deadline_view

--- Course/views.py
+++ Course/views.py
@@ -11,44 +11,6 @@ from django.db import IntegrityError
 
 def signup(request):
     return render(request, "index.html")
-    '''
-    user = request.user
-    if user.is_authenticated():
-        
-        if (request.method == "POST"):
-            notifyEmail = request.POST.get("notifyEmail")
-            if not request.POST.get("settings"):
-                user_profile = UserProfile()
-                user_profile.user = user
-            else:
-                user_profile = request.user.get_profile()
-            if notifyEmail:
-                user_profile.notify_email = notifyEmail
-            else:
-                user_profile.notify_email = user.email
-            if not request.POST.get("notifications") == "Allow":
-                user_profile.notifications = False
-            else:
-                user_profile.notifications = True
-            user_profile.save()
-            return HttpResponseRedirect("/")
-            
-        try:
-            user_profile = request.user.get_profile()
-            return render(request, "index.html", {"user_profile" : user_profile})
-        except UserProfile.DoesNotExist:
-            return render(request, "firstLogin.html")
-                
-        
-    return render(request, "index.html")
-    '''
-def settings(request):
-    user = request.user
-    if user.is_authenticated():
-        user_profile = request.user.get_profile()
-        return render(request, "settings.html", {"user_profile" : user_profile})
-    else:
-        raise Http404
         
 
 def chart(request):
